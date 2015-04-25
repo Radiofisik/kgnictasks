dd18d7611307f09211b198932c91b9c5fa49f5f3
Radiofisik <mail@radiofisik.ru>
2015-04-25 23:50:30 +0200

    full calendar added

--- root/frontend/views/site/index.php
+++ root/frontend/views/site/index.php
@@ -3,47 +3,77 @@
 $this->title = 'My Yii Application';
 ?>
 <div class="site-index">
-
-    <div class="jumbotron">
-        <h1>Congratulations!</h1>
-
-        <p class="lead">You have successfully created your Yii-powered application.</p>
-
-        <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>
-    </div>
-
     <div class="body-content">
 
         <div class="row">
-            <div class="col-lg-4">
-                <h2>Heading</h2>
+		
+		
+		
+		
+		
+	<?php
+	
+	
+	class Event1
+	{
+		public $id;
+		public $title;
+		public $start;
+		public $end;
+		public $textColor;
+		public $allDay;
+	}
+	
+	
+$events = array();
+  //Testing
+  $Event = new Event1();
+  $Event->id = 1;
+  $Event->title = 'Событие 1';
+  $Event->start = date('Y-m-d\TH:m:s\Z');
+  $Event->end = date('Y-m-d\TH:m:s\Z',strtotime('Monday this week'));
+  $Event->textColor='yellow';
+  $Event->allDay=true;
+  $events[] = $Event;
+/*
+  $Event = new Event1();
+  $Event->id = 2;
+  $Event->title = 'Testing';
+  $Event->start = date('Y-m-d\TH:m:s\Z',strtotime('tomorrow 6am'));
+  $events[] = $Event;*/
+?>  
+		
+		
+		
+		<?= \talma\widgets\FullCalendar::widget([
+	
+    'googleCalendar' => false,  // If the plugin displays a Google Calendar. Default false
+    'loading' => 'Загрузка...', // Text for loading alert. Default 'Loading...'
+    'config' => [
+	'events'=> $events,
+        // put your options and callbacks here
+        // see http://arshaw.com/fullcalendar/docs/
+        'lang' => 'ru', // optional, if empty get app language
+		'dayClick' => new \yii\web\JsExpression("function(date, jsEvent, view) {alert(date);}"),
+     
+    ],
+]); ?>
 
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
 
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
+		
+		
+		
 
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/forum/">Yii Forum &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a></p>
+		
+		
+		
+		
+		
+		
+		
+		
+		
+          
             </div>
         </div>
 
